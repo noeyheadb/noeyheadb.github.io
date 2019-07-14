c2f2f92773ee32bf1cdf7f03a1f9088289d2a92c
Daehyeon Bae <noeyheadb@gmail.com>
2019-07-14 21:46:27 +0900

    [dev]Feat: Add scroll animation

--- js/home.js
+++ js/home.js
@@ -2,3 +2,7 @@ function github_click()
 {
     window.open('https://github.com/noeyheadb', '_blank').focus();
 }
+
+function go(target_id) {
+    (new MoveTo()).move(document.getElementById(target_id));
+}
