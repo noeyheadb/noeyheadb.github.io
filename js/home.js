4f78dcb02a04d3fa50cb63ece76d4f70967392b6
DaeHyeon Bae <noeyheadb@gmail.com>
2020-08-08 15:25:25 +0900

    ✨ Apply theme

--- js/home.js
+++
@@ -1,8 +0,0 @@
-function github_click()
-{
-    window.open('https://github.com/noeyheadb', '_blank').focus();
-}
-
-function go(target_id) {
-    (new MoveTo()).move(document.getElementById(target_id));
-}
